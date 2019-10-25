f7e3c53ab3935fef72f92105d79d0df7e93d622e
Alice Won <alicewon4@gmail.com>
2019-10-25 00:02:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,8 @@ def adding_matz
         }
      }
 
-programmer_hash.unshift(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
-programmer_hash[0]
+programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
+
 end
 
 def changing_alan
